c56124af76b420b353c9326c26594cdae06779d1
kiran_a <iamkiranashok@gmail.com>
2022-12-15 15:35:27 +0530

    second commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    String name;
+    int age;
+    double salary;
+    String location;
+
+
+    Employee(String name,int age,double salary,String location)
+    {
+        this.name=name;
+        this.age=age;
+        this.salary=salary;
+        this.location=location;
+    }
+//create a function salary raise//
+    void raiseSalary() {
+        this.salary = this.salary * 2000;
+    }
 }
